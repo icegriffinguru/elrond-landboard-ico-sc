e9465bf6f9429c06c54875d62416584322e42c37
icegriffinguru <icegriffin1122@gmail.com>
2022-02-21 17:19:24 +0300

    0.0.4 - testnet.sh

--- interaction/testnet.snippets.sh
+++ interaction/testnet.snippets.sh
@@ -1,10 +1,32 @@
-ALICE="${USERS}/alice.pem"
-ADDRESS=$(erdpy data load --key=address-testnet)
-DEPLOY_TRANSACTION=$(erdpy data load --key=deployTransaction-testnet)
-PROXY=https://testnet-api.elrond.com
+##### - configuration - #####
+NETWORK_NAME="testnet" # devnet, testnet, mainnet
+PROXY=https://testnet-gateway.elrond.com
+CHAIN_ID="T"
+
+WALLET="./wallets/testnet-wallet.pem" # main actor pem file
+
+TOKEN_NAME="LAND-123456"
+LOCKED_TOKEN_NAME="LKLAND-123456"
+
+TOKEN_NAME_HEX="0x$(echo -n ${TOKEN_NAME} | xxd -p -u | tr -d '\n')"
+LOCKED_TOKEN_NAME_HEX="0x$(echo -n ${LOCKED_TOKEN_NAME} | xxd -p -u | tr -d '\n')"
+TOKEN_PRICE=200000000000000
+MIN_BUY_LIMIT=200000000000000000
+MAX_BUY_LIMIT=1000000000000000000
 
 deploy() {
-    erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${ALICE} --gas-limit=50000000 --arguments 0 --send --outfile="deploy-testnet.interaction.json" --proxy=${PROXY} --chain=T || return
+    erdpy --verbose contract deploy \
+    --project=${PROJECT} \
+    --recall-nonce \
+    --pem=${WALLET} \
+    --gas-limit=100000000 \
+    --arguments ${TOKEN_NAME_HEX} ${LOCKED_TOKEN_NAME_HEX} ${TOKEN_PRICE} ${MIN_BUY_LIMIT} ${MAX_BUY_LIMIT} \
+    --send \
+    --outfile="deploy-testnet.interaction.json" \
+    --proxy=${PROXY} \
+    --metadata-payable \
+    --metadata-payable-by-sc \
+    --chain=${CHAIN_ID} || return
 
     TRANSACTION=$(erdpy data parse --file="deploy-testnet.interaction.json" --expression="data['emitted_tx']['hash']")
     ADDRESS=$(erdpy data parse --file="deploy-testnet.interaction.json" --expression="data['emitted_tx']['address']")
@@ -14,13 +36,4 @@ deploy() {
 
     echo ""
     echo "Smart contract address: ${ADDRESS}"
-}
-
-add() {
-    read -p "Enter number: " NUMBER
-    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=5000000 --function="add" --arguments ${NUMBER} --send --proxy=${PROXY} --chain=T
-}
-
-getSum() {
-    erdpy --verbose contract query ${ADDRESS} --function="getSum" --proxy=${PROXY}
 }
